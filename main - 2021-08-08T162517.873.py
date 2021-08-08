e8c8db82b7877cf0cf7a12093a750d79c07b9482
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-08 16:27:41 -0400

    Add files via upload

---
+++ main - 2021-08-08T162517.873.py
@@ -0,0 +1,67 @@
+print("Welcome to Dhruv's Cruptocurrency Converter!")
+val=str(input('Enter the output cryptocurrency code:'))
+
+if(val=='BTC'):
+    USD=float(input('Enter the amount in USD:'))
+    BTC=USD*0.000022
+        print('%0.3f USD is equal to %0.3f bitcoin' %(USD, BTC))
+elif(val=='ETH'):
+    USD=float(input('Enter the amount in USD:'))
+    ETH=USD*0.000321
+        print('%0.3f USD is equal to %0.3f ethereum' %(USD, ETH))
+elif(val=='LTC'):
+    USD=float(input('Enter the amount in USD:'))
+    LTC=USD*0.0064623264
+        print('%0.3f USD is equal to %0.3f litecoin' %(USD, LTC))
+elif(val=='ADA'):
+    USD=float(input('Enter the amount in USD:'))
+    ADA=USD*0.681243
+        print('%0.3f USD is equal to %0.3f cardano' %(USD, ADA))
+elif(val=='DOGE'):
+    USD=float(input('Enter the amount in USD:'))
+    DOGE=USD*4.757766
+        print('%0.3f USD is equal to %0.3f dogecoin' %(USD, DOGE))
+elif(val=='USDT'):
+    USD=float(input('Enter the amount in USD:'))
+    USDT=USD*1
+        print('%0.3f USD is equal to %0.3f Tether' %(USD, DOGE))
+elif(val=='USDC'):
+    USD=float(input('Enter the amount in USD:'))
+    USDC=USD*1.00040016
+        print('%0.3f USD is equal to %0.3f USDC' %(USD, USDC))
+elif(val=='DOT'):
+    USD=float(input('Enter the amount in USD:'))
+    DOT=USD*0.0489955904
+        print('%0.3f USD is equal to %0.3f USDC' %(USD, DOT))
+elif(val=='UNI'):
+    USD=float(input('Enter the amount in USD:'))
+    UNI=USD*0.0373831776
+        print('%0.3f USD is equal to %0.3f UNI' %(USD, UNI))
+elif(val=='WBTC'):
+    USD=float(input('Enter the amount in USD:'))
+    WBTC=USD*0.0000226775316
+        print('%0.3f USD is equal to %0.3f WBTC' %(USD, WBTC))
+elif(val=='XLM'):
+    USD=float(input('Enter the amount in USD:'))
+    XLM=USD*3.4106412
+        print('%0.3f USD is equal to %0.3f XLM' %(USD, XLM))
+elif(val=='LINK'):
+    USD=float(input('Enter the amount in USD:'))
+    LINK=USD*0.0423908436
+        print('%0.3f USD is equal to %0.3f LINK' %(USD, LINK))
+elif(val=='ICP'):
+    USD=float(input('Enter the amount in USD:'))
+    ICP=USD*0.0180603215
+        print('%0.3f USD is equal to %0.3f ICP' %(USD, ICP))
+elif(val=='MATIC'):
+    USD=float(input('Enter the amount in USD:'))
+    MATIC=USD*0.900900901
+        print('%0.3f USD is equal to %0.3f MATIC' %(USD, MATIC))
+elif(val=='THETA'):
+    USD=float(input('Enter the amount in USD:'))
+    THETA=USD*0.156739812
+        print('%0.3f USD is equal to %0.3f Theta' %(USD, THETA))
+elif(val=='FIL'):
+    USD=float(input('Enter the amount in USD:'))
+    FIL=USD*0.0152905199
+        print('%0.3f USD is equal to %0.3f Filecoin' %(USD, FIL))  
